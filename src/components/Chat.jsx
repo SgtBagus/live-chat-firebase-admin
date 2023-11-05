145ecdb8a13f732e3775f4fad2331dd7bc8f749a
Bagus Andika <SgtBagus>
2023-11-05 13:21:48 +0700

    add enable disabled button

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,25 +1,79 @@
-import React, { useContext } from "react";
-import { ChatContext } from "../context/ChatContext";
+import React, { Component } from "react";
+import { doc, onSnapshot, updateDoc } from "firebase/firestore";
 
 import Messages from "./Messages";
 import Input from "./Input";
-import Navbar from "./Navbar";
 
-const Chat = () => {
-  const { data } = useContext(ChatContext);
+import { db } from "../firebase";
 
-  return (
-    <div className="chat">
-      <div className="chatInfo">
-        <span>{data.user?.displayName}</span>
-        <div className="sidebar-message">
-          <Navbar />
+class Chat extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messageStatus: false,
+    };
+  }
+
+  componentDidUpdate = (prevProps) => {
+    const { dataChat } = this.props;
+    const { dataChat: pDataChat } = prevProps;
+
+    if (pDataChat !== dataChat) {
+      this.getStatusMessage(dataChat.chatId);
+    }
+  }
+
+  getStatusMessage = async (id) => {
+    try {
+      await onSnapshot(doc(db, "chats", id), (doc) => {
+        if (doc.exists()) {
+          const { allow_chat: allowChat } = doc.data();
+
+          this.setState({
+            messageStatus: allowChat,
+          })
+        }
+      });
+    } catch (err) {
+      alert(err);
+    }
+  }
+
+  changeStatusMessage = async (value) => {
+    const { dataChat: { chatId: id } } = this.props;
+    try {
+      await updateDoc(doc(db, "chats", id), {
+        allow_chat: value,
+      });
+    } catch(err) {
+      alert(err);
+    }
+  }
+  
+  render() {
+    const { messageStatus } = this.state;
+    const { dataChat, currentUser } = this.props;
+
+    return (
+      <div className="chat">
+        <div className="chatInfo">
+          <span>{dataChat.user?.displayName}</span>
+          <div>
+            {
+              !messageStatus ? (
+                <button onClick={() => this.changeStatusMessage(true)}>Enable Chat</button>
+              ) : (
+                <button onClick={() => this.changeStatusMessage(false)}>Disabled Chat</button>
+              )
+            }
+          </div>
         </div>
+        <Messages />
+        <Input dataChat={dataChat} currentUser={currentUser} disabledText={messageStatus} />
       </div>
-      <Messages />
-      <Input/>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Chat;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { ChatContext } from "../context/ChatContext";
+import { AuthContext } from "../context/AuthContext";
 
 import Sidebar from '../components/Sidebar'
 import Chat from '../components/Chat'
 
 const Home = () => {
+  const { data } = useContext(ChatContext);
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <div className='home'>
       <div className="container">
-        <Sidebar/>
-        <Chat/>
+        <Sidebar />
+        <Chat dataChat={data} currentUser={currentUser} />
       </div>
     </div>
   )
